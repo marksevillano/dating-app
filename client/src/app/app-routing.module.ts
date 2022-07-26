b0d7d3b268c8b9979ed057d074d423f592680bae
Mark Sevillano <sevillano.markanthony.e@gmail.com>
2022-07-26 20:59:13 +0800

    Commit for Section 9: Building the User Interface

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemberListComponent, canActivate:[AuthGuard]},
-      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'members/:username', component: MemberDetailComponent },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
